057f4e061cc97302f69d63ba26bfbb9e5bd04987
Akhil Goel <akhil1goel@gmail.com>
2022-05-25 11:28:44 +0530

    Debug Unrequired "," & Bullet Point Display Error.

--- jsFiles/set-data.js
+++ jsFiles/set-data.js
@@ -11,9 +11,11 @@ for (let key in data1.workHistory) {
         listString += `<li> ${workDescription[i]} </li>`;
     };
 
-    let workDiv = document.createElement('div');
-    workDiv.innerHTML = `<h3 class="date" id="role" > ${data1.workHistory[key]["role"]} ,</h3> <p class="date" id="date" > ${data1.workHistory[key]["date"]}</p><p id="location" > ${data1.workHistory[key]["location"]}</p><ul class="work-history--list" id="description">${listString}</ul>`;
-    let workHistoryDiv = document.getElementById("work-history");workHistoryDiv.appendChild(workDiv);
+    if (data1.workHistory[key]["role"] !== ''){
+        let workDiv = document.createElement('div');
+        workDiv.innerHTML = `<h3 class="date" id="role" > ${data1.workHistory[key]["role"]} ,</h3> <p class="date" id="date" > ${data1.workHistory[key]["date"]}</p><p id="location" > ${data1.workHistory[key]["location"]}</p><ul class="work-history--list" id="description">${listString}</ul>`;
+        let workHistoryDiv = document.getElementById("work-history");workHistoryDiv.appendChild(workDiv);
+    };
     
 }
 
@@ -24,9 +26,11 @@ document.getElementById("email").innerHTML = "&nbsp;&nbsp;" + data1.email;
 let skillsArray = (data1.skills).split('\n');
 let skillsUL = document.getElementById("skillsUL");
 for (i in skillsArray) {
-    let skillsArrayLi = document.createElement('li');
-    skillsArrayLi.innerHTML = skillsArray[i];
-    skillsUL.appendChild(skillsArrayLi);
+    if (skillsArray[i] !== '') {
+        let skillsArrayLi = document.createElement('li');
+        skillsArrayLi.innerHTML = skillsArray[i];
+        skillsUL.appendChild(skillsArrayLi);
+    }
 }
 
 document.getElementById("degree").innerHTML = data1.degree;
@@ -35,7 +39,9 @@ document.getElementById("university").innerHTML = data1.college;
 let affiliationArray = (data1.affiliation).split('\n');
 let affiliationUL = document.getElementById("affiliationUL");
 for (i in affiliationArray) {
-    let affiliaionArrayLi = document.createElement('li');
-    affiliaionArrayLi.innerHTML = affiliationArray[i];
-    affiliationUL.appendChild(affiliaionArrayLi);
+    if (affiliationArray[i] !== '') {
+        let affiliaionArrayLi = document.createElement('li');
+        affiliaionArrayLi.innerHTML = affiliationArray[i];
+        affiliationUL.appendChild(affiliaionArrayLi);
+    };
 }
