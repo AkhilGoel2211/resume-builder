4344625fe0b4b7b81b8042d9b7e3813bd735d546
Akhil Goel <akhil1goel@gmail.com>
2022-05-25 21:53:42 +0530

    Added Template Selection.

--- jsFiles/button-click.js
+++ jsFiles/button-click.js
@@ -1,48 +1,13 @@
 const init = function(e) {
 	let btn1 = document.querySelector("#item1");
 	let btn2 = document.querySelector("#item2");
-	let data = {}
 
 	btn1.addEventListener('click', function() {
-		data = {
-			firstName: document.getElementById("firstName").value,
-			lastName: document.getElementById("lastName").value,
-			summary: document.getElementById("summary").value,
-			workHistory: {
-				history1: {
-					role: document.getElementById("role1").value,
-					date: document.getElementById("date1").value,
-					description: document.getElementById("description1").value,
-					location: document.getElementById("location1").value,
-				},
-				history2: {
-					role: document.getElementById("role2").value,
-					date: document.getElementById("date2").value,
-					description: document.getElementById("description2").value,
-					location: document.getElementById("location2").value,
-				},
-				history3: {
-					role: document.getElementById("role3").value,
-					date: document.getElementById("date3").value,
-					description: document.getElementById("description3").value,
-					location: document.getElementById("location3").value,
-				},
-			},
-			adress: document.getElementById("adress").value,
-			number: document.getElementById("number").value,
-			email: document.getElementById("email").value,
-			skills: document.getElementById("skills").value,
-			degree: document.getElementById("education--degree").value,
-			college: document.getElementById("education--college").value,
-			affiliation: document.getElementById("affiliation").value,
-		};
-		console.log(data);
-		localStorage.setItem('data', JSON.stringify(data));
-		window.document.location = './TemplateOne/template-one.html';
+		window.document.location = './TemplateOne/template-one-form.html';
 
 	});
 	btn2.addEventListener('click', function() {
-		window.document.location = '/TemplateTwo/template-two.html';
+		window.document.location = './TemplateTwo/template-two-form.html';
 	});
 };
 
---
+++ jsFiles/submit-click.js
@@ -0,0 +1,54 @@
+const init = function(e) {
+    let submit1 = document.querySelector("#submit1");
+    let submit2 = document.querySelector("#submit2");
+    let data1 = {};
+    let data2 = {};
+
+    submit1.addEventListener('click', function() {
+		data1 = {
+			firstName: document.getElementById("firstName").value,
+			lastName: document.getElementById("lastName").value,
+			summary: document.getElementById("summary").value,
+			workHistory: {
+				history1: {
+					role: document.getElementById("role1").value,
+					date: document.getElementById("date1").value,
+					description: document.getElementById("description1").value,
+					location: document.getElementById("location1").value,
+				},
+				history2: {
+					role: document.getElementById("role2").value,
+					date: document.getElementById("date2").value,
+					description: document.getElementById("description2").value,
+					location: document.getElementById("location2").value,
+				},
+				history3: {
+					role: document.getElementById("role3").value,
+					date: document.getElementById("date3").value,
+					description: document.getElementById("description3").value,
+					location: document.getElementById("location3").value,
+				},
+			},
+			adress: document.getElementById("adress").value,
+			number: document.getElementById("number").value,
+			email: document.getElementById("email").value,
+			skills: document.getElementById("skills").value,
+			degree: document.getElementById("education--degree").value,
+			college: document.getElementById("education--college").value,
+			affiliation: document.getElementById("affiliation").value,
+		};
+		console.log(data1);
+		localStorage.setItem('data', JSON.stringify(data1));
+		window.document.location = 'template-one.html';
+	});
+
+    submit2.addEventListener('click', function() {
+		data2 = {};
+		console.log(data2);
+		localStorage.setItem('data', JSON.stringify(data2));
+		window.document.location = 'template-two.html';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	init();});
