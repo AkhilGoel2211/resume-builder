708a36325e3bf074e83a3cd0d46aff159ee12eff
Akhil Goel <akhil1goel@gmail.com>
2022-05-25 15:46:08 +0530

    Implement PDF Generation.

--- jsFiles/generatePDF.js
+++ jsFiles/generatePDF.js
@@ -1,11 +1,28 @@
+function Convert_HTML_To_PDF() {
+  let btnElement = document.getElementById("generatePDF");
+  btnElement.remove();
+  window.print();
+  document.body.appendChild(btnElement);
+}
+
+
+
 // let doc = new jsPDF();
 // let specialElementHandlers = {
 //     '#pdf': function (element, renderer) {
 //         return true;
 //     }
 // };
-    
+
+
+// function Convert_HTML_To_PDF() {
+//   let pdf = new jsPDF('p', 'pt', 'a4');
+//   pdf.addHTML($('#container'))[0], function() {
+//     pdf.save('Test.pdf');
+//   }
+// }
      
+
 // $('#generatePDF').click(function () {
 //     doc.fromHTML($('#container').html(), 15, 15, {
 //         'width': 880,
@@ -54,46 +71,53 @@
 // });
 
 
-function Convert_HTML_To_PDF() {
-    var elementHTML = document.getElementById('container');
+// function Convert_HTML_To_PDF() {
+//     var elementHTML = document.getElementById('container');  
+//     html2canvas(elementHTML, {
+//       useCORS: true,
+//       onrendered: function(canvas) {
+//         var pdf = new jsPDF('p', 'pt', 'a4');
+//         let pageHeight = 980;
+//         let pageWidth = 900;
+//         for (let i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
+//           let srcImg = canvas;
+//           let sX = 0;
+//           let sY = pageHeight * i; // start 1 pageHeight down for every new page
+//           let sWidth = pageWidth;
+//           let sHeight = pageHeight;
+//           let dX = 0;
+//           let dY = 0;
+//           let dWidth = pageWidth;
+//           let dHeight = pageHeight;
   
-    html2canvas(elementHTML, {
-      useCORS: true,
-      onrendered: function(canvas) {
-        var pdf = new jsPDF('p', 'pt', 'a4');
+//           window.onePageCanvas = document.createElement("canvas");
+//           onePageCanvas.setAttribute('width', pageWidth);
+//           onePageCanvas.setAttribute('height', pageHeight);
+//           var ctx = onePageCanvas.getContext('2d');
+//           ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
   
-        let pageHeight = 980;
-        let pageWidth = 900;
-        for (let i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
-          let srcImg = canvas;
-          let sX = 0;
-          let sY = pageHeight * i; // start 1 pageHeight down for every new page
-          let sWidth = pageWidth;
-          let sHeight = pageHeight;
-          let dX = 0;
-          let dY = 0;
-          let dWidth = pageWidth;
-          let dHeight = pageHeight;
+//           var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+//           var width = onePageCanvas.width;
+//           var height = onePageCanvas.clientHeight;
   
-          window.onePageCanvas = document.createElement("canvas");
-          onePageCanvas.setAttribute('width', pageWidth);
-          onePageCanvas.setAttribute('height', pageHeight);
-          var ctx = onePageCanvas.getContext('2d');
-          ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+//           if (i > 0) // if we're on anything other than the first page, add another page
+//             pdf.addPage(595, 841); // 8.5" x 12" in pts (inches*72)
   
-          var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
-          var width = onePageCanvas.width;
-          var height = onePageCanvas.clientHeight;
-  
-          if (i > 0) // if we're on anything other than the first page, add another page
-            pdf.addPage(595, 841); // 8.5" x 12" in pts (inches*72)
-  
-          pdf.setPage(i + 1); // now we declare that we're working on that page
-          pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
-        }
+//           pdf.setPage(i + 1); // now we declare that we're working on that page
+//           pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
+//         }
               
-        // Save the PDF
-        pdf.save('document.pdf');
-      }
-    });
-  }
+//         // Save the PDF
+//         pdf.save('document.pdf');
+//       }
+//     });
+//   }
+
+
+// function printDiv(divName) {
+//   var printContents = document.getElementById(divName).innerHTML;
+//   var originalContents = document.body.innerHTML;
+//   document.body.innerHTML = printContents;
+//   window.print();
+//   document.body.innerHTML = originalContents;
+// }
